ffff0e8b145ac8d856ac9edb2a0c5b2e22035126
Christopher Jack Turner <chris.turner137@gmail.com>
2018-01-03 18:49:33 +0000

    Improved 'add' command to save all the entries returned into an SQLite database if they are new. Improved 'search' command to dump all entries from the database.

--- chi_feed.py
+++ chi_feed.py
@@ -13,9 +13,12 @@ Options:
   -h --help  Show this help.
 """
 from docopt import docopt
+import sys
 import os, errno
 import json
 import feedparser
+import sqlite3
+from contextlib import closing
 
 def load_feeds_config():
   if not os.path.exists('.chi/feed'):
@@ -47,27 +50,95 @@ def command_feed_init(args):
 
   raise NotImplementedError
 
+def db_configure(connection):
+  """ Ensure that the database is valid. """
+  with closing(connection.cursor()) as cursor:
+    try:
+      cursor.execute('CREATE TABLE IF NOT EXISTS `entries` (id STRING UNIQUE, dump STRING);')
+    except:
+      raise NotImplementedError
+
+  # TODO: Check that the format of the tables is as expected.
+  connection.commit()
+
 def command_feed_add(args):
   """ Add an RSS feed to track. """
-  feed = feedparser.parse(args['<source>'])
-  print(feed)
-  raise NotImplementedError
+  if not os.path.exists('.chi/feed'):
+    raise NotImplementedError
+
+  # Fetch and parse the RSS feed
+  try:
+    feed = feedparser.parse(args['<source>'])
+  except:
+    raise NotImplementedError
+
+  # Dumps the feed description to stdout
+  print(feed.feed)
+
+  # Add new entries to the database
+  with sqlite3.connect('.chi/feed/entries.db') as connection:
+    # Ensure that the database is initialised
+    db_configure(connection)
+
+    # Add all entries where there is no contrain violation.
+    with closing(connection.cursor()) as cursor:
+      added = 0
+      for entry in feed['entries']:
+        try:
+          cursor.execute('INSERT INTO `entries` VALUES (?,?);',
+                         (entry['id'], json.dumps(entry)))
+          added += 0
+        except sqlite3.DatabaseError as e:
+          # Ignore attempts to add duplicate entries
+          if e.args[0] != 'UNIQUE constraint failed: entries.id':
+            raise
+          print('Ignored duplicate entry with id "{}"'.format(entry.id))
+        except sqlite3.DatabaseError as e:
+          print(e)
+          raise
+      print('Entries added', added)
+      connection.commit()
 
 def command_feed_list(args):
   """ List tracked RSS feeds. """
   feeds = load_feeds_config()
   print(feeds)
+  return 0
 
 def command_feed_fetch(args):
   """ Fetch articles from RSS feeds. """
+  if not os.path.exists('.chi/feed'):
+    raise NotImplementedError
   raise NotImplementedError
 
 def command_feed_flow(args):
   """ Process articles from RSS feeds. """
+  if not os.path.exists('.chi/feed'):
+    raise NotImplementedError
   raise NotImplementedError
 
 def command_feed_search(args):
   """ Search database of articles built from RSS feeds. """
+  if not os.path.exists('.chi/feed'):
+    raise NotImplementedError
+
+  with sqlite3.connect('.chi/feed/entries.db') as connection:
+    # Ensure that the database is initialised
+    db_configure(connection)
+
+    # Perform the search
+    print('Entries:')
+    with closing(connection.cursor()) as cursor:
+      connection.commit()
+      try:
+        cursor.execute('SELECT * FROM `entries`;')
+        i = 0
+        for i, row in enumerate(cursor):
+          print(row)
+        print("Row(s) affected: ", i)
+      except Exception as e:
+        raise NotImplementedError
+
   raise NotImplementedError
 
 def command_feed(args):
@@ -90,4 +161,5 @@ if __name__ == '__main__':
   # Parse command line arguments
   args = docopt(__doc__, version='chi-feed 1.0')
 
-  command_feed(args)
+  retval = command_feed(args)
+  sys.exit(retval)
---
+++ hardreset.sh
@@ -0,0 +1,1 @@
+rm .chi/feed/entries.db
